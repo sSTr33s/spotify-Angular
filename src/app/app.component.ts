4c0c7ef264e1e81b776ecfb9c4af5305ddb66026
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-16 14:41:07 -0500

    primer vistazo a TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spotify';
+
+  status: string | number = 0
+
+  name: string = 'spotify'
+  age: number = 28
+  phone: null = null
+  phone1: undefined = undefined
+  datum: any //Puede ser cualquier cosa
+
+
+  //Interfaz 
+  car:CarModel = {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2017
+  }
+
+  car2:CarModel = {
+    brand: 'Ford',
+    model: 'Mustang',
+    
+  }
+
+  listCars:Array<CarModel> =[{
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2017
+  },
+  {
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 2017
+  }
+  ,
+  {
+    brand: 'Ford',
+    model: 'Mustang',
+
+  }
+]
+    
+}
+//Es un contrato con un tipo de datos 
+interface CarModel {
+  brand: string
+  model: string
+  year?: number
 }
