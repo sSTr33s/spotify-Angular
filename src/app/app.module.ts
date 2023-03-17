e6cb622ab956efccc8810c01edfa4bece5942d6e
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-17 09:58:52 -0500

    Modularizando angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MediaPlayerComponent } from './shared/components/media-player/media-player.component';
 
 @NgModule({
   declarations: [ //TODO: Declaraciones, componentes, directivas, pipes 
-    AppComponent, MediaPlayerComponent
+    AppComponent
   ],
   imports: [//TODO: Solo se importan otros modules
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-
-
 
 @NgModule({
   declarations: [
     HeaderUserComponent,
-    SideBarComponent
+    SideBarComponent,
+    MediaPlayerComponent,
   ],
   imports: [
     CommonModule
   ]
 })
-export class SharedModule { }
+export class SharedModule { 
+    
+}
