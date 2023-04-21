839568431e5de7018eb706d7d3722800e527686c
Ss_Tr33s <24_de_7@protonmail.com>
2023-04-20 22:53:42 -0500

    Pause and play ended

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -9,20 +9,34 @@ import { Subscribable, Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
+
   mockCover!: TracksModule;
 
-  listOberservs!: Array<Subscription>;
+  state: string = 'paused'
+  
+  listOberservs$!: Array<Subscription>;
 
-  constructor(public multimediaService: MultimediaService) {}
+  constructor(public multimediaService: MultimediaService) { }
 
   ngOnInit() {
+
     this.multimediaService.trackInfo$.subscribe((trackInfo) => {
       console.log('Debo reproducir esta canción', trackInfo);
-      this.mockCover=trackInfo;
+      this.mockCover = trackInfo;
     });
+
+    const observer1$=this.multimediaService.playerStatus$
+    .subscribe(status=>{
+      this.state = status
+    });
+
+    this.listOberservs$ = [observer1$]
   }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this.listOberservs$.forEach(u => u.unsubscribe())
+    console.log('🔴🔴🔴🔴🔴🔴🔴 BOOM!');
+  }
   /*
   ngOnInit() {
     const oberser1: Subscription = this.multimediaService.callback.subscribe(
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -7,26 +7,94 @@ import { TracksModule } from '../../core/models/tracks.model';
 }) //
 
 export class MultimediaService {
- 
   callback: EventEmitter<any> = new EventEmitter<any>();
 
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
+  public timeElapsed$: BehaviorSubject<any> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<any> = new BehaviorSubject('-00:00');
+  public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused')
+
   public audio!: HTMLAudioElement;
 
-  constructor()
-  {
+  constructor() {
     this.audio = new Audio();
+
     this.trackInfo$.subscribe((track: TracksModule) => {
       if (track) {
         this.setAudio(track);
       }
     });
+
+    this.listenAllEvents();
   }
 
-  private setAudio(track: TracksModule): void 
-  {
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);
+    this.audio.addEventListener('playing', this.setPlayerStatus, false);
+    this.audio.addEventListener('play', this.setPlayerStatus, false);
+    this.audio.addEventListener('pause', this.setPlayerStatus, false);
+    this.audio.addEventListener('ended', this.setPlayerStatus, false);
+  }
+
+  private setPlayerStatus = (event: any) => {
+    //console.table({ event });
+    switch (event.type) {
+
+      case 'play':
+        this.playerStatus$.next('play');
+        break;
+
+      case 'playing':
+        this.playerStatus$.next('playing');
+        break;
+
+      case 'ended':
+        this.playerStatus$.next('ended');
+        break;
+
+      default:
+        this.playerStatus$.next('paused')
+
+    }
+  }
+
+  private calculateTime = () => {
+
+    // console.log('reproduciendo');
+    const { duration, currentTime } = this.audio;
+
+    //console.table({ duration, currentTime });
+    this.setTimeElapsed(currentTime);
+    this.setTimeRemaining(currentTime, duration);
+  }
+
+  private setTimeElapsed(currentTime: number): void {
+
+    this.timeElapsed$.next(this.displayFormatedDuration(currentTime));
+  }
+
+  private setTimeRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+    this.timeRemaining$.next(this.displayFormatedDuration(timeLeft));
+  }
+
+  private displayFormatedDuration(currentTime: number): string {
+    let seconds = Math.floor(currentTime % 60);
+    let minutes = Math.floor((currentTime / 60) % 60);
+
+    let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    let displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${displayMinutes}:${displaySeconds}`;
+  }
+
+  private setAudio(track: TracksModule): void {
     this.audio.src = track.url;
     this.audio.play();
+  }
+
+  public togglePlayer(): void {
+    (this.audio.paused) ? this.audio.play() : this.audio.pause()
   }
 }
